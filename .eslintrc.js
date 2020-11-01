b907ae3ed9aa866cc77d92b6e2cbbccb48081a4e
lmachens <leon.machens@googlemail.com>
2020-11-01 08:15:41 +0100

    Add ESLint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': 'off',
+    'prettier/prettier': 'warn',
+  },
+};
