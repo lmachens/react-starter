1e1016f362ba993c1a807957851c9e2c07387ff0
lmachens <leon.machens@googlemail.com>
2020-11-01 12:51:14 +0100

    Restructure files

--- src/pages/Page.js
+++ src/pages/Page.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Header from './Header';
+import Header from '../components/Header';
 import './page.css';
 
 const Page = ({ user, onLogin, onLogout, onCreateAccount }) => (
